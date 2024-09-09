babde3c0c0a0dab85aa324ceeee3ce0dae079a37
[Muqadas Khan] <[muqadask2021@gmail.com]>
2024-09-09 19:33:33 -0400

    TestNG

---
+++ src/test/java/com/testng/training/NewTest2.java
@@ -0,0 +1,10 @@
+package com.testng.training;
+
+import org.testng.annotations.Test;
+
+public class NewTest2 extends TestNGExample{
+  @Test
+	  public void test2() {
+	        System.out.println("This is my second  test using TestNG"); 
+  }
+  }
---
+++ src/test/java/com/testng/training2/Test2.java
@@ -0,0 +1,23 @@
+package com.testng.training2;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+  @Test
+  public void test4() {
+      System.out.println("This is my fourt test using TestNG"); 
+  }
+
+  @Test
+  public void test5() {
+      System.out.println("This is my fifth test using TestNG");
+  }
+
+  @Test
+  public void test6() {
+      System.out.println("This is my sixth test using TestNG");
+  }
+}
+
+
+
